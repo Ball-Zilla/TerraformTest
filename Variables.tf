4e372d90ecbc4aaa3da65ad24c541d283339770e
Ball-Zilla <goat@gmail.com>
2025-08-07 23:18:09 +0000

    another one

--- Variables.tf
+++ Variables.tf
@@ -1,20 +1,20 @@
-# variable "vpc_cidr_block" {
-#   description = "This is the CIDR for the VPC"
-# }
+variable "vpc_cidr_block" {
+  description = "This is the CIDR for the VPC"
+}
 
-# variable "subnet1_cidr_block" {
-#   description = "This is the subnet1 CIDR block"
-# }
+variable "subnet1_cidr_block" {
+  description = "This is the subnet1 CIDR block"
+}
 
-# variable "subnet2_cidr_block" {
-#   description = "This is the subnet2 CIDR block"
-# }
+variable "subnet2_cidr_block" {
+  description = "This is the subnet2 CIDR block"
+}
 
-# variable "igw_cidr_block" {
-#     description = "This is the CIDR block for the internet gateway"
+variable "igw_cidr_block" {
+    description = "This is the CIDR block for the internet gateway"
   
-# }
+}
 
-# variable "region" {
-#   description = "This is the AWS region of the resources"
-# }
+variable "region" {
+  description = "This is the AWS region of the resources"
+}
